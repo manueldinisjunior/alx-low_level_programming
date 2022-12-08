5617f818100fccc19ab4021e0cd9079da8ff9721
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2022-12-08 17:41:09 +0200

    fire

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
+ * main - prints a text
+ *
+ * Return: Always 0 (successful)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
