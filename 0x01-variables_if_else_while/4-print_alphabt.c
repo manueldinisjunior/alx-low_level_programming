df5ffad432e790835a6f85cef513e90801030dc0
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2022-12-09 14:34:51 +0200

    Print alphabet c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * main - Entry block
+ * Description: lowercase without q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
