695bbecfe61fb2b76a9217b89472093c954bc7b1
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2022-12-15 05:44:40 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,32 +1,12 @@
 #include "main.h"
 
 /**
- *  * more_numbers - prints more numbers
- *   * Return:void
- *    */
-
-void more_numbers(void)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-		char n, c;
-			int i = 0;
-
-
-				while (i < 10)
-						{
-									for (n = 0; n <= 14; n++)
-												{
-																c = n;
-																			if (n > 9)
-																							{
-																												_putchar('1');
-																																c = n % 10;
-																																			}
-
-																						_putchar('0' + c);
-																								}
-
-											_putchar('\n');
-													i++;
-														}
-
+    more_numbers();
+    return (0);
 }
