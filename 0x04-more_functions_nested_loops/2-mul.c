67d4cc9f52a5b5b4fadfd73871cb44ebd1287f5a
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2022-12-15 21:28:37 +0200

    Mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/**
- *  * mul - returns the product of its parameters
- *   * @a: int type number
- *    * @b: int type number
- *     * Return:0
- *      */
 
+/**
+ * mul - multiplies 2 nums
+ * @a: first param
+ * @b: second param
+ * Return: something
+ */
 
 int mul(int a, int b)
 {
