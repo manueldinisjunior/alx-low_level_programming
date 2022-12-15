c9784ae7f25c3132f5e8d8d61784b1ca0a12fe25
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2022-12-15 21:30:32 +0200

    Print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,34 +1,30 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_triangle - check for a digit
- * @size : integer type
- * Return:void
+ * print_triangle - create triangle
+ * @size: params
+ * Return: 0
  */
 
 void print_triangle(int size)
 {
+	int i = 0, j, n = size - 1;
 
-	int i = 1, ii;
-
-	while (i <= size && size > 0)
+	if (size > 0)
 	{
-		ii = 0;
-		while (ii < size - i)
+		for (; i < size; i++)
 		{
-			_putchar(' ');
-			ii++;
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
 		}
-		ii = 0;
-		while (ii < i)
-		{
-			_putchar('#');
-			ii++;
-		}
-
-		_putchar('\n');
-		i++;
 	}
-	if (i == 1)
+	else
 		_putchar('\n');
 }
