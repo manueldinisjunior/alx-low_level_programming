29d3865a2266cb5801cb223889b42bf07686aced
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2022-12-21 18:02:54 +0200

    🌍🏆🇲🇿🔥

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _main_h_
+#define _main_h_
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -10,9 +10,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
 
 #endif
