a6977d2fab25e2ea284bf4d50bf28cead69da6f1
manueldinisjunior <manueldinisjunior@gmail.com>
2022-12-21 07:29:08 -0800

    It's getting weird

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,26 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates two strings
- * @dest: pointer destination
- * @src: source of pointer
- * Return: void
- */
+ *  * _strcat - concatenates two strings,
+ *   * @dest: destination.
+ *    * @src: source.
+ *     * Return: the pointer to dest.
+ *      */
 char *_strcat(char *dest, char *src)
 {
-	int a = -1, i;
+		int count = 0, count2 = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	;
-	do {
-		a++;
-		dest[i] = src[a];
-		i++;
-	} while (src[a] != '\0');
+			while (*(dest + count) != '\0')
+					{
+								count++;
+									}
 
-	return (dest);
+				while (count2 >= 0)
+						{
+									*(dest + count) = *(src + count2);
+											if (*(src + count2) == '\0')
+															break;
+													count++;
+															count2++;
+																}
+					return (dest);
 }
