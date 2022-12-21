d114fcf8d16eaca63c8bfb8a19c626a31249fd0c
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2022-12-21 17:44:15 +0200

    concatenates two strings 🌍🏆🇲🇿🔥

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,20 @@
 #include "main.h"
+
 /**
- *  * _strcat - concatenates two strings,
- *   * @dest: destination.
- *    * @src: source.
- *     * Return: the pointer to dest.
- *      */
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
+ */
+
 char *_strcat(char *dest, char *src)
 {
-		int count = 0, count2 = 0;
+	int index = 0;
+	int dest_len = 0;
 
-			while (*(dest + count) != '\0')
-					{
-								count++;
-									}
-
-				while (count2 >= 0)
-						{
-									*(dest + count) = *(src + count2);
-											if (*(src + count2) == '\0')
-															break;
-													count++;
-															count2++;
-																}
-					return (dest);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
