5f5375a83349518d76e15e62b6093090bd5b4a73
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-01-09 15:35:13 +0200

    Main

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,12 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
 
-/**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-malloc_free directory.
- */
-
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
