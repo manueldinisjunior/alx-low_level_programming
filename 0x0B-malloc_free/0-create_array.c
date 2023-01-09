db0ab5381462da46540abe5c786a622bce5f1895
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-01-09 15:18:59 +0200

    CA

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,28 +1,30 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * create_array - create an array of chart and initialize
- * it with a specificc char.
- * @size: array size
- * @c: char to initialize arrray with
- * Return:NULL if 0
+ * create_array - Creates an array of chars and
+ *                initializes it with a specific char.
+ * @size: The size of the array to be initialized.
+ * @c: The specific char to intialize the array with.
+ *
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
  */
-
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *a;
+	char *array;
+	unsigned int index;
 
 	if (size == 0)
 		return (NULL);
 
-	a = malloc(size * sizeof(char));
+	array = malloc(sizeof(char) * size);
 
-	if (a == NULL)
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		a[i] = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-	return (a);
+	return (array);
 }
