a837cb9ce8b2d6b7dcdae6456fdc35306b3fa3f1
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-01-09 15:20:03 +0200

    Putchar

--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * _putchar - write the character c to stdout
- * @c: the character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
  * Return: On success 1.
- * On error, -1 is returned, and error is set app.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
