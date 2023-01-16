5e7125971bcf7462072de509f412f87adb6342fa
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-01-16 14:59:35 +0200

    added protptypes to dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,26 +2,26 @@
 #define DOG_H
 
 /*
- *  * File: dog.h
- *   * Desc: Header file that defines a new type struct dog.
- *    */
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
 
 /**
- *  * struct dog - A new type describing a dog.
- *   * @name: The name of the dog.
- *    * @age: The age of the dog.
- *     * @owner: The owner of the dog.
- *      */
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
- *  * dog_t - Typedef for struct dog
- *   */
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
