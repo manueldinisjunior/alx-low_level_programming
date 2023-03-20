ae3156544c2ca23823c5186aafc7fe29904d3f9b
manueldinisjunior <manueldinisjunior@gmail.com>
2023-03-20 12:13:04 -0700

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: string to be processed
+ * @src: string to be processed
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = src[i];
+	return (&dest[0]);
+}
