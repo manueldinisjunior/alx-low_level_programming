54f04e950749e2ba7cfd315ecb3b301acbac1b03
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-03-25 07:39:06 +0200

    Dynamic Foolery

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,9 @@
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 #include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -8,7 +11,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -21,4 +24,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* DYNAMIC_LIB */
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+extern div_t div(int __numer, int __denom);
+
+#endif /* MAIN_H */
